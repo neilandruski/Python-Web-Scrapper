1a71de9ba5d886aa8f0834dfe668bf3858db5a12
Neil Andruski <neil.andruski@gmail.com>
2023-03-23 13:54:49 -0400

    Got selenium working for javascript pages.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Python-Web-Scrapper
 Python script thats use an existing csv to parse a list of printer IPs and grab the toner levels.
+
+Versalink models had issues with the page renderer from request. Selenium needed to be used to render the javascript page.
+
--- web-scrapper.py
+++ web-scrapper.py
@@ -1,9 +1,40 @@
 #!/usr/bin/python3
 
-import pandas, requests, datetime, urllib3
+import pandas, requests, datetime, urllib3, time
+
+from selenium import webdriver 
+from selenium.webdriver import Chrome 
+from selenium.webdriver.chrome.service import Service 
+from selenium.webdriver.common.by import By 
+from webdriver_manager.chrome import ChromeDriverManager
+
 import sys
 
 from bs4 import BeautifulSoup as bs
+
+# web scrapping code from 
+# https://www.zenrows.com/blog/scraping-javascript-rendered-web-pages#installing-the-requirements
+# start by defining the options 
+options = webdriver.ChromeOptions() 
+options.add_argument('--headless') # it's more scalable to work in headless mode 
+# normally, selenium waits for all resources to download 
+# we don't need it as the page also populated with the running javascript code. 
+options.page_load_strategy = 'none' 
+
+# this returns the path web driver downloaded 
+chrome_path = ChromeDriverManager().install() 
+chrome_service = Service(chrome_path) 
+# pass the defined options and service objects to initialize the web driver 
+driver = Chrome(options=options, service=chrome_service) 
+driver.implicitly_wait(3)
+
+urllib3.disable_warnings()
+
+'''
+--------------------------------------------------------------------------------
+FUNCTIONS
+--------------------------------------------------------------------------------
+'''
 
 #Parse Altalink page for toner levels
 def get_altalink_levels(ip):
@@ -27,38 +58,35 @@ def get_versalink_levels(ip):
     URL = "http://" + ip + "/home/index.html#hashHome"
     print ("testing URL: " + URL)
     try:
-        response = requests.get(URL, verify=False)
+        driver.get(URL)
+        time.sleep(10)
     except:
         print("Can't reach " + ip + ", Please check the printer")
         return []
-    soup = bs(response.content, "html.parser")
-    #print(soup)
-    job_elements = soup.find_all("li", class_="webui-home-media-text")
+    content = driver.find_elements(By.CSS_SELECTOR, "li[class*='webui-home-media-text'")
+    if (len(content) == 0 ):
+        return []
     levels = []
-    for item in job_elements:
-        levels.append(float(item.text))
+    levels.append(float(content[-1].text.strip("%")))
     return levels
 
-
+'''
+--------------------------------------------------------------------------------
+Main Function loop
+--------------------------------------------------------------------------------
+'''
 #Pull in intial file for IPs to scrap
-urllib3.disable_warnings()
 df = pandas.read_csv('test.csv')
-
-#Testing webpage load for versalink
-#levels = get_versalink_levels("10.248.50.9") 
-#sys.exit()
 
 #Loop through IPs and get levels. Set the Levels in Pandas and update csv
 printer_level_list = []
 levels = []
 for index, row in df.iterrows():
     if ("AltaLink" in row['Model']):
-        #levels = get_altalink_levels(row['ip'].strip())
-        continue
+        levels = get_altalink_levels(row['ip'].strip())
     if ("VersaLink" in row['Model']):
         levels = get_versalink_levels(row['ip'].strip()) 
     printer_level_list.append(levels)
-
 
 #Testing that the Printers returned levels
 print (printer_level_list)
